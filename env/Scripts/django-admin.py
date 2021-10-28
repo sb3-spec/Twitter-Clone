de66500804c98278571add9847e4676262b443be
Robert Sheffield <sheffira2@gmail.com>
2021-10-27 19:54:09 -0700

    First Commit

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\sheff\Documents\Fullstack Projects\twitter_clone_back\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
