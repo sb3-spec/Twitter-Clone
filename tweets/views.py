b3d9cd90455051a992bd3a99f219e44bc522053d
Robert Sheffield <sheffira2@gmail.com>
2021-11-02 10:26:27 -0700

    Tweets now append to top of home page, tweet creation is live, tweets being saved to database

--- tweets/views.py
+++ tweets/views.py
@@ -1,6 +1,66 @@
-from django.shortcuts import render
+from django.http.response import Http404, JsonResponse
+from django.shortcuts import render, redirect
+from django.conf import settings
 from django.http import HttpResponse
+from django.utils.http import is_safe_url
+import random 
+
+from .models import Tweet
+from .forms import TweetForm
+
+ALLOWED_HOSTS = settings.ALLOWED_HOSTS
 
 # Create your views here.
 def home_view(request, *args, **kwargs):
-    return HttpResponse("<h1>Hello World</h1>")
+    # return HttpResponse("<h1>Hello World</h1>")
+    return render(request, "pages/home.html", context={})
+
+def tweet_create_view(request, *args, **kwargs):
+    """Creates form view for creating tweets"""
+    form = TweetForm(request.POST or None)
+    next_url = request.POST.get("next") or None
+    if form.is_valid():
+        obj = form.save(commit=False)
+        obj.save()
+        print(request.is_ajax())
+        if request.is_ajax():
+            return JsonResponse(obj.serialize(), status=201)
+        
+        if is_safe_url(next_url, ALLOWED_HOSTS):
+            return redirect(next_url)
+        
+        form = TweetForm()
+    return render(request, 'components/form.html', context={"form": form})
+
+
+def tweet_list_view(request, *args, **kwargs):
+    qs = Tweet.objects.all()
+    tweets_list = [tweet.serialize() for tweet in qs]
+    data = {
+        "isUser": False,
+        "response": tweets_list
+    }
+    
+    return JsonResponse(data)
+    
+    
+    
+def tweet_detail_view(request, tweet_id, *args, **kwargs):
+    """
+    REST API VIEW 
+    Consume by JavaScript 
+    """
+    data = {
+        "id": tweet_id,
+        # "image_path": obj.image.url
+    }
+    status = 200
+    try:
+        obj = Tweet.objects.get(id=tweet_id)
+        data['content'] = obj.content
+    except:
+        data.message('Not Found')
+        status = 400
+    
+    
+    return JsonResponse(data, status=status) # json.dumps
--- twitter_clone_back/settings.py
+++ twitter_clone_back/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'no^e7hxhokj8j-o9nlk82()qst6k8-(+m!4w(w01mbm!&669tk'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'twitter_clone_back.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
