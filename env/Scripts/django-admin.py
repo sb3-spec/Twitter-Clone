61252469b9c28c9fdcc61e6231441dafe7c9efb0
Robert Sheffield <68346980+sb3-spec@users.noreply.github.com>
2021-12-20 20:51:17 -0800

    Delete env directory

--- env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\sheff\Documents\Fullstack Projects\twitter_clone_back\env\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
